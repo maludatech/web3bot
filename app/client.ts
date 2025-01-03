400158bbf07119d30066c425e5277548343c7d67
maluda <nwitevictor7@gmail.com>
2025-01-03 02:31:04 +0100

    fixed env error

--- app/client.ts
+++ app/client.ts
@@ -1,6 +1,6 @@
 import { createThirdwebClient, CreateThirdwebClientOptions } from "thirdweb";
 
-const clientId = process.env.CLIENT_ID as string;
+const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
 
 if (!clientId) {
   throw new Error("CLIENT_ID is not defined in environment variables.");
