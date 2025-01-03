2ff37c254cf5a048536c3ae8bc3af2650f9ece45
maluda <nwitevictor7@gmail.com>
2025-01-03 03:43:44 +0100

    added aunthication

--- components/RockPaperScissor.tsx
+++ components/RockPaperScissor.tsx
@@ -64,56 +64,63 @@ export default function RockPaperScissors() {
     <div className="flex items-center justify-center h-full w-full bg-[#f0f0f0] text-[#333]">
       <div className="p-10 m-10 w-[400px] max-w-[98%] h-[400px] bg-white rounded-lg shadow-black shadow-lg flex flex-col items-center justify-start relative">
         <h1 className="text-xl  font-bold mb-10 text-center">Mini Game</h1>
-        <ConnectButton
-          client={client}
-          wallets={[
-            inAppWallet({
-              auth: {
-                options: ["email"],
-              },
-            }),
-          ]}
-        />
-        <div>
-          {!result ? (
+        {!account ? (
+          <ConnectButton
+            client={client}
+            wallets={[
+              inAppWallet({
+                auth: {
+                  options: ["email"],
+                },
+              }),
+            ]}
+          />
+        ) : (
+          <>
             <div>
-              <h3 className="font-bold pt-4">Choose your option:</h3>
-              <div className="flex justify-center gap-5 m-10">
-                {choices.map((choice) => (
-                  <button
-                    key={choice}
-                    onClick={() => handleChoice(choice)}
-                    className="p-5 bg-[#007bff] text-white border-none rounded-md text-xl cursor-pointer "
-                  >
-                    {choice === "Rock"
-                      ? "🪨"
-                      : choice === "Paper"
-                      ? "📄 "
-                      : "✂ "}
-                  </button>
-                ))}
-              </div>
+              {!result ? (
+                <div>
+                  <h3 className="font-bold pt-4">Choose your option:</h3>
+                  <div className="flex justify-center gap-5 m-10">
+                    {choices.map((choice) => (
+                      <button
+                        key={choice}
+                        onClick={() => handleChoice(choice)}
+                        className="p-5 bg-[#007bff] text-white border-none rounded-md text-xl cursor-pointer "
+                      >
+                        {choice === "Rock"
+                          ? "🪨"
+                          : choice === "Paper"
+                          ? "📄 "
+                          : "✂ "}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              ) : (
+                <div className="flex flex-col items-center relative">
+                  <p className="text-xl mb-5">
+                    You chose: {result.playerChoice}
+                  </p>
+                  <p className="text-xl mb-5">
+                    Computer Choice: {result.computerChoice}
+                  </p>
+                  <p className="text-xl mb-5 font-extrabold">
+                    Result: {result.gameResult}
+                  </p>
+                  <div className="absolute bottom-4 flex flex-col gap-4 items-center">
+                    <button
+                      className="w-full p-3 bg-[#28a745] text-sm text-white border-none rounded-md cursor-pointer"
+                      onClick={resetGame}
+                    >
+                      Try again
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
-          ) : (
-            <div className="flex flex-col items-center relative">
-              <p className="text-xl mb-5">You chose: {result.playerChoice}</p>
-              <p className="text-xl mb-5">
-                Computer Choice: {result.computerChoice}
-              </p>
-              <p className="text-xl mb-5 font-extrabold">
-                Result: {result.gameResult}
-              </p>
-              <div className="absolute bottom-4 flex flex-col gap-4 items-center">
-                <button
-                  className="w-full p-3 bg-[#28a745] text-sm text-white border-none rounded-md cursor-pointer"
-                  onClick={resetGame}
-                >
-                  Try again
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
